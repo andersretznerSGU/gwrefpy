f9dfbdcd513adba19ad7b7b697ab8a7aca046d86
eriktoller <erik@toller.se>
2025-08-20 20:33:46 +0200

    Docs update

--- docs/conf.py
+++ docs/conf.py
@@ -45,7 +45,7 @@ master_doc = "index"
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_book_theme"
+html_theme = "pydata_sphinx_theme"
 html_title = "AnDFN"
 html_theme_options = {
     "navbar_align": "content",
