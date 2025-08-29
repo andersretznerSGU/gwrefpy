b92ad3a1b5c3c2ef11f651b6d37dae0add95b97d
Anders Retzner <anders.retzner@gmail.com>
2025-08-29 11:19:25 +0200

    add strandangers model as a test case

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,7 @@
-import pandas as pd
 from pathlib import Path
+from gwrefpy import Model, Well
+
+import pandas as pd
 import pytest
 
 
@@ -12,3 +14,23 @@ assert TEST_PATH.exists(), f"Test file {TEST_PATH} does not exist."
 def timeseries() -> pd.Series:
     """Fixture for loading a timeseries for testing."""
     return pd.read_csv(TEST_PATH, parse_dates=True, index_col=0).squeeze()
+
+
+@pytest.fixture()
+def strandangers_model() -> Model:
+    """Fixture for creating Strandangers test case."""
+    dates = pd.date_range("2020-01-01", periods=3, freq="D")
+    values_obs = [11.4, 11.7, 11.8]
+    values_ref = [8.9, 9.2, 9.4]
+
+    obs = Well(name="obs", is_reference=False)
+    obs.add_timeseries(pd.Series(values_obs, index=dates))
+
+    ref = Well(name="ref", is_reference=True)
+    ref.add_timeseries(pd.Series(values_ref, index=dates))
+
+    model = Model(name="Strandangers")
+    model.add_well(obs)
+    model.add_well(ref)
+
+    return model
