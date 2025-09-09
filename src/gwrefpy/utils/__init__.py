0fe5c58c53bdd2def491260316426ab907b3afd9
eriktoller <erik@toller.se>
2025-09-09 10:53:38 +0200

    Added logger_config.py

--- src/gwrefpy/utils/__init__.py
+++ src/gwrefpy/utils/__init__.py
@@ -1,3 +1,4 @@
 from .decorators import print_return, timed
+from . import logger_config
 
 __all__ = ["timed", "print_return"]
---
+++ src/gwrefpy/utils/logger_config.py
@@ -0,0 +1,43 @@
+import logging.config
+
+# Configure logging
+LOGGING_CONFIG = {
+        'version': 1,
+        'disable_existing_loggers': True,
+        'formatters': {
+            'standard': {
+                'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+            },
+            'minimal': {
+                'format': '%(message)s'
+            },
+        },
+        'handlers': {
+            'default': {
+                'level': 'INFO',
+                'formatter': 'minimal',
+                'class': 'logging.StreamHandler',
+                'stream': 'ext://sys.stdout',
+            },
+            'file': {
+                'level': 'INFO',
+                'formatter': 'standard',
+                'class': 'logging.FileHandler',
+                'filename': 'gwrefpy.log',
+                'mode': 'w',
+                'encoding': 'utf-8',
+            },
+        },
+        'loggers': {
+            '': {  # root logger
+                'handlers': ['default', 'file'],
+                'level': 'INFO',
+                'propagate': True
+            },
+        }
+    }
+
+logging.config.dictConfig(LOGGING_CONFIG)
+
+logger = logging.getLogger(__name__)
+logger.info("Logging is configured.")
