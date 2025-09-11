c19744a7cc3a2764b8d9f01390ba42eab08eab8d
eriktoller <erik@toller.se>
2025-09-11 14:12:58 +0200

    Made log file optional, @officialankan happy?

--- src/gwrefpy/utils/logger_config.py
+++ src/gwrefpy/utils/logger_config.py
@@ -14,19 +14,11 @@ LOGGING_CONFIG = {
             "formatter": "minimal",
             "class": "logging.StreamHandler",
             "stream": "ext://sys.stdout",
-        },
-        "file": {
-            "level": "INFO",
-            "formatter": "standard",
-            "class": "logging.FileHandler",
-            "filename": "gwrefpy.log",
-            "mode": "w",
-            "encoding": "utf-8",
-        },
+        }
     },
     "loggers": {
         "": {  # root logger
-            "handlers": ["default", "file"],
+            "handlers": ["default"],
             "level": "INFO",
             "propagate": True,
         },
@@ -34,7 +26,6 @@ LOGGING_CONFIG = {
 }
 
 logging.config.dictConfig(LOGGING_CONFIG)
-
 logger = logging.getLogger(__name__)
 logger.debug("Logging is configured.")
 
@@ -51,3 +42,29 @@ def set_log_level(level: str) -> None:
         raise ValueError(f"Invalid log level: {level}")
     logging.getLogger("gwrefpy").setLevel(numeric_level)
     logger.info(f"Log level set to {level}")
+
+
+def enable_file_logging(name: str = "gwrefpy.log", filemode: str = "w") -> None:
+    """Enable logging to a file named 'gwrefpy.log' in the current directory.
+
+    Parameters
+    ----------
+    name : str
+        The name of the log file. Default is 'gwrefpy.log'.
+    filemode : str
+        The mode to open the log file. Default is 'w' (write mode).
+        Other common mode is 'a' (append mode).
+    """
+    root_logger = logging.getLogger()
+    # check if the name has an extension, if not add .log
+    if not name.endswith(".log"):
+        name += ".log"
+    # Check if file handler already exists
+    if not any(isinstance(h, logging.FileHandler) for h in root_logger.handlers):
+        file_handler = logging.FileHandler(name, mode=filemode, encoding="utf-8")
+        formatter = logging.Formatter(
+            "%(asctime)s [%(levelname)s] %(name)s: %(message)s"
+        )
+        file_handler.setFormatter(formatter)
+        file_handler.setLevel(logging.DEBUG)
+        root_logger.addHandler(file_handler)
